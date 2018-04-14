349cd153d745d9cdc86d5bf6a98413e6cb51a14d
mg2005 <warrior.catcake@gmail.com>
2018-04-14 14:47:20 -0700

    second commit

---
+++ Add Homework Screen Design/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Add Homework Screen Design
+//
+//  Created by Mahika Gupta on 3/11/18.
+//  Copyright © 2018 Mahika Gupta. All rights reserved.
+//
+
+import UIKit
+
+class AddHomework: UIViewController {
+
+    
+    var homeworkname: String = ""
+    
+    @IBOutlet weak var homeworkTitle: UITextField!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBAction func DoneButton(_ sender: Any) {
+        homeworkname = homeworkTitle.text!
+        titleLabel.text = "\(homeworkname)"
+    }
+    
+}
+
