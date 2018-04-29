85e834144583e23cfe80b78a78bbe053c3e03e39
mg2005 <warrior.catcake@gmail.com>
2018-04-29 10:46:03 -0700

    Removed labels

--- Add Homework Screen Design/ViewController.swift
+++ Add Homework Screen Design/ViewController.swift
@@ -13,61 +13,47 @@ class AddHomework: UIViewController {
     //Homework Title
     var homeworkName: String = ""
     @IBOutlet weak var homeworkTitle: UITextField!
-    @IBOutlet weak var titleLabel: UILabel!
+   
     
     //Class Name
     var className: String = ""
     @IBOutlet weak var classNameTextField: UITextField!
-    @IBOutlet weak var classNameLabel: UILabel!
     
     //Teacher
     var teacher: String = ""
     @IBOutlet weak var teacherTextField: UITextField!
-    @IBOutlet weak var teacherLabel: UILabel!
     
     //Assigned Date
     var assignedDate: String = ""
     @IBOutlet weak var assignedDateTextField: UITextField!
-    @IBOutlet weak var assignedDateLabel: UILabel!
     
     //Due Date
     var dueDate: String = ""
     @IBOutlet weak var dueDateTextField: UITextField!
-    @IBOutlet weak var dueDateLabel: UILabel!
     
     //Description
     var descriptiontextbox: String = ""
     @IBOutlet weak var descriptionTextField: UITextField!
-    @IBOutlet weak var descriptionLabel: UILabel!
     
     //Other
     var other: String = ""
     @IBOutlet weak var otherTextField: UITextField!
-    @IBOutlet weak var otherLabel: UILabel!
     
     //DONE
     @IBAction func DoneButton(_ sender: Any) {
         homeworkName = homeworkTitle.text!
-        titleLabel.text = "\(homeworkName)"
         
         className = classNameTextField.text!
-        classNameLabel.text = "\(className)"
         
         teacher = teacherTextField.text!
-        teacherLabel.text = "\(teacher)"
         
         assignedDate = assignedDateTextField.text!
-        assignedDateLabel.text = "\(assignedDate)"
         
         dueDate = dueDateTextField.text!
-        dueDateLabel.text = "\(dueDate)"
         
         descriptiontextbox = descriptionTextField.text!
-        descriptionLabel.text = "\(descriptiontextbox)"
         
-        other = otherTextField.text!
-        otherLabel.text = "\(other)"
-        
+        other = otherTextField.text!        
     }
     
     
